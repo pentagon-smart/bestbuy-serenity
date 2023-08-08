f05e847f93f673ed47ad1efc8887e29c3df64c66
pentagon-smart <pentagonsmart26@gmail.com>
2023-08-08 17:15:49 +0100

    BestBuy Serenity Project1

---
+++ src/main/java/com/bestbuy/constants/Path.java
@@ -0,0 +1,14 @@
+package com.bestbuy.constants;
+
+
+public class Path {
+    /*
+     * This is base path for best buy api
+     */
+    public static final String PRODUCTS = "/products";
+    public static final String STORES = "/stores";
+
+
+
+
+}
